5912539fe0cf83cc900fde4c374489feef8ea0f8
Gustavo Toporowicz <gh.toporowicz@gmail.com>
2020-03-11 01:41:26 -0300

    add functions to validate the matrix

--- src/tests/validator.test.js
+++ src/tests/validator.test.js
@@ -1,11 +1,26 @@
 const validator = require('../validator')
 
-test('validate a square matrix', () => {
+test('A square matrix', () => {
     const matrix = [[1, 4, 1], [0, 6, 4], [0, 0, 1]];
     expect(validator.confirmSquareMatrix(matrix)).toBe(true);
 });
 
-test('validate a not square matrix', () => {
+test('A not square matrix', () => {
     const matrix = [[1, 0, 0], [2, 8, 0], [4, 9, 7, 9]];
     expect(validator.confirmSquareMatrix(matrix)).toBe(false);
 });
+
+test('The top of the matrix has only zeros', () => {
+    const matrix = [[1, 0, 0], [2, 8, 0], [4, 9, 7]];
+    expect(validator.UpperValidate(matrix)).toBe(true);
+});
+
+test('The bottom of the matrix has only zeros', () => {
+    const matrix = [[1, 4, 1], [0, 6, 4], [0, 0, 1]];
+    expect(validator.LowerValidate(matrix)).toBe(true);
+});
+
+test('Diagonal of the matrix is ​​correct', () => {
+    const matrix = [[1, 4, 1], [0, 1, 4], [0, 0, 1]];
+    expect(validator.diagonalValidate(matrix)).toBe(true);
+}); 
--- src/validator.js
+++ src/validator.js
@@ -1,8 +1,47 @@
-const confirmSquareMatrix = (matrix) => {
+const confirmSquareMatrix = matrix => {
     return !matrix.find(row => row.length !== matrix.length);
 }
 
+const UpperValidate = matrix => {
+    const sizeRow = matrix.length
+    for(let i = 0 ; i < sizeRow ; i++) {
+        for(let j = 0 ; j < sizeRow ; j++) {
+            if(i < j && matrix[i][j] != 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+const LowerValidate = matrix => {
+    const sizeRow = matrix.length
+    for(let i = 0 ; i < sizeRow ; i++) {
+        for(let j = 0 ; j < sizeRow ; j++) {
+            if(i > j && matrix[i][j] != 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+const diagonalValidate = matrix => {
+    const sizeRow = matrix.length
+    for(let i = 0 ; i < sizeRow ; i++) {
+        for(let j = 0 ; j < sizeRow ; j++) {
+            if(i == j && matrix[i][j] == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 module.exports = {
-    confirmSquareMatrix
+    confirmSquareMatrix,
+    UpperValidate,
+    LowerValidate,
+    diagonalValidate
 }
 
