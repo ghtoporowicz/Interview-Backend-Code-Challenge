ca4f7585cc99dc90bab77e6531128d14bc019a6e
Gustavo Toporowicz <gh.toporowicz@gmail.com>
2020-03-11 02:14:06 -0300

    fix validator to validate zeros in the other part of the matrix

--- src/tests/validator.test.js
+++ src/tests/validator.test.js
@@ -1,26 +1,31 @@
 const validator = require('../validator')
 
-test('A square matrix', () => {
-    const matrix = [[1, 4, 1], [0, 6, 4], [0, 0, 1]];
-    expect(validator.confirmSquareMatrix(matrix)).toBe(true);
-});
-
 test('A not square matrix', () => {
     const matrix = [[1, 0, 0], [2, 8, 0], [4, 9, 7, 9]];
-    expect(validator.confirmSquareMatrix(matrix)).toBe(false);
+    expect(validator.triangularMatrixValidate(matrix)).toBe(false);
 });
 
-test('The top of the matrix has only zeros', () => {
-    const matrix = [[1, 0, 0], [2, 8, 0], [4, 9, 7]];
-    expect(validator.UpperValidate(matrix)).toBe(true);
-});
-
-test('The bottom of the matrix has only zeros', () => {
+test('Confirms that it is a triangular matrix (UPPER)', () => {
     const matrix = [[1, 4, 1], [0, 6, 4], [0, 0, 1]];
-    expect(validator.LowerValidate(matrix)).toBe(true);
+    expect(validator.triangularMatrixValidate(matrix)).toBe(true);
 });
 
-test('Diagonal of the matrix is ​​correct', () => {
-    const matrix = [[1, 4, 1], [0, 1, 4], [0, 0, 1]];
-    expect(validator.diagonalValidate(matrix)).toBe(true);
-}); 
+test('Confirms that it is a triangular matrix (LOWER)', () => {
+    const matrix = [[1, 0, 0], [2, 8, 0], [4, 9, 7]];
+    expect(validator.triangularMatrixValidate(matrix)).toBe(true);
+});
+
+test('Confirms that it is a triangular matrix (diagonal incorrect)', () => {
+    const matrix = [[1, 0, 0], [2, 0, 0], [4, 1, 7]];
+    expect(validator.triangularMatrixValidate(matrix)).toBe(false);
+});
+ 
+test('Confirms that it is a triangular matrix (lower incorrect)', () => {
+    const matrix = [[1, 0, 0], [2, 8, 0], [4, 0, 7]];
+    expect(validator.triangularMatrixValidate(matrix)).toBe(false);
+});
+
+test('Confirms that it is a triangular matrix (lower incorrect)', () => {
+    const matrix = [[1, 0, 1], [0, 6, 4], [0, 0, 1]];
+    expect(validator.triangularMatrixValidate(matrix)).toBe(false);
+});
--- src/validator.js
+++ src/validator.js
@@ -2,30 +2,6 @@ const confirmSquareMatrix = matrix => {
     return !matrix.find(row => row.length !== matrix.length);
 }
 
-const UpperValidate = matrix => {
-    const sizeRow = matrix.length
-    for(let i = 0 ; i < sizeRow ; i++) {
-        for(let j = 0 ; j < sizeRow ; j++) {
-            if(i < j && matrix[i][j] != 0) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-const LowerValidate = matrix => {
-    const sizeRow = matrix.length
-    for(let i = 0 ; i < sizeRow ; i++) {
-        for(let j = 0 ; j < sizeRow ; j++) {
-            if(i > j && matrix[i][j] != 0) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
 const diagonalValidate = matrix => {
     const sizeRow = matrix.length
     for(let i = 0 ; i < sizeRow ; i++) {
@@ -35,13 +11,58 @@ const diagonalValidate = matrix => {
             }
         }
     }
+
     return true;
 }
 
+const isLowerMatrix = (upper, lower) => {
+    if(!upper.find(value => value != 0) && !lower.includes(0)) {
+        console.log('[INFO] This matrix can be Lower');
+        return true;
+    }
+    return false;
+
+}
+
+const isUpperMatrix = (upper, lower) => {
+    if(!lower.find(value => value != 0) && !upper.includes(0)) {
+        console.log('[INFO] This matrix can be Upper');
+        return true;
+    }
+    return false;
+}
+
+const triangularMatrixValidate = matrix => {
+    if(!confirmSquareMatrix(matrix)) {
+        console.log('[ERROR] Is not a square Matrix');
+        return false;
+    }
+    
+    let upper = [];
+    let lower = [];
+
+    if(!diagonalValidate(matrix)) {
+        console.log('[ERROR] Diagonal contains zeros');
+        return false;
+    }
+
+    for(let i = 0 ; i < matrix.length ; i++) {
+        for(let j = 0 ; j < matrix.length ; j++) {
+            if(i < j) {
+                upper.push(matrix[i][j]);
+            }
+
+            if(i > j) {
+                lower.push(matrix[i][j]);
+            }
+        }
+    }
+
+    return isLowerMatrix(upper, lower) || isUpperMatrix(upper, lower);
+}
+
+
 module.exports = {
-    confirmSquareMatrix,
-    UpperValidate,
-    LowerValidate,
-    diagonalValidate
+    triangularMatrixValidate
 }
 
